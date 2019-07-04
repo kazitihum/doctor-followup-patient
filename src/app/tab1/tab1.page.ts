e3ce640478307e294af7beb29ece39b2b78c789a
Kazi Tihum <kazitihum@gmail.com>
2019-07-04 12:46:42 +0000

    Homepage update

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,64 +4,82 @@ import { ApiService } from './../services/api.service';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Storage } from '@ionic/storage';
-
+import { async } from '@angular/core/testing';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
 
-  docData: Observable<any>;
-  searchTerm: string = '';
+    userInfo: any;
+    patient_id: any;
+    age: any;
+    docData: Observable<any>;
+    searchTerm: string = '';
 
-  constructor( private storage: Storage,private apiService: ApiService, private passdata: PassdataService, private router: Router) {
+    constructor(private storage: Storage, private apiService: ApiService, private passdata: PassdataService, private router: Router) {
 
-    
-    this.storage.get('user_info').then((data) =>{
-      console.log(data);
-    })
-   }
 
-  doctorSearch(name) {
+        this.storage.get('user_info').then((data) => {
+            console.log(data);
+        })
+    }
 
-    console.log(name);
-    this.apiService.doctorSearch(this.searchTerm).subscribe(
-      data => {
+    async ngOnInit() {
+        await this.storage.get('user_info').then(async (data) => {
 
-        console.log(data);
-        this.docData = data;
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
+            let bdate = new Date(data.user_info.birth_date);
+            let timeDiff = Math.abs(Date.now() - bdate.getTime());
+            this.age = Math.floor((timeDiff / (1000 * 3600 * 24)) / 365.25);
 
-  doctorDetails(data) {
+            this.patient_id = await (data.user_info.id);
+            this.userInfo = data.user_info;
 
-    this.passdata.setData('docdetails', data);
-    this.router.navigateByUrl('/doctor-profile');
-  }
 
-  logout(){
-    this.storage.clear().then((data)=>{
-      console.log(data);
-    });
-    this.router.navigateByUrl('/login');
-    
-    // this.apiService.logout().subscribe(
-    //   data => {
+            console.log(bdate.getTime() + ' ', bdate + " ", this.age);
+        })
+    }
 
-    //     console.log(data);
-    //     this.router.navigateByUrl('/login');
-    //   },
-    //   error => {
-    //     console.log(error);
-    //   }
-    // );
-  }
+    doctorSearch(name) {
+
+        console.log(name);
+        this.apiService.doctorSearch(this.searchTerm).subscribe(
+            data => {
+
+                console.log(data);
+                this.docData = data;
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
+
+    doctorDetails(data) {
+
+        this.passdata.setData('docdetails', data);
+        this.router.navigateByUrl('/doctor-profile');
+    }
+
+    logout() {
+        this.storage.clear().then((data) => {
+            console.log(data);
+        });
+        this.router.navigateByUrl('/login');
+
+        // this.apiService.logout().subscribe(
+        //   data => {
+
+        //     console.log(data);
+        //     this.router.navigateByUrl('/login');
+        //   },
+        //   error => {
+        //     console.log(error);
+        //   }
+        // );
+    }
 
 
 }
